89ef306859a4a66074f603c4af112383d1c92d6c
Bhavin <stark.bhavin@gmail.com>
2024-02-17 15:31:21 +0530

    Problem Comments adding

---
+++ frontend/src/Components/Login.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import "../Styles/Login.css"
+
+function Login() {
+  return (
+    <div>
+      {/* Login page code here */}
+    </div>
+  )
+}
+
+export default Login
---
+++ frontend/src/Components/Signup.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import "../Styles/Signup.css"
+
+function Signup() {
+  return (
+    <div>
+        {/* Signup page here */}
+    </div>
+  )
+}
+
+export default Signup
