c55b9f5bbe7f73920b7b211f71f85e7a7ab91e42
Bhavin <stark.bhavin@gmail.com>
2024-02-24 19:43:51 +0530

    New

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,9 @@
 import './App.css';
 import React, { useEffect, useState } from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import axios from 'axios';
 import Login from './Components/Login';
+import Home from './Components/Home'
 
 function App() {
   const [data, setData] = useState(null)
@@ -13,9 +15,14 @@ function App() {
   }, []);
   
   return (
-    <div className="App">
-      <Login />
-    </div>
+    <Router>
+      <div className="App">
+        <Routes>
+          <Route exact path="/" Component={Home} />
+          <Route path="/login" Component={Login} />
+        </Routes>        
+      </div>
+    </Router>
   );
 }
 
