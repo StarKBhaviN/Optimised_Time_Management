d0528e1fee8f150549bf719e026a5736389724e2
krupali1105 <krupalibhagat1155@gmail.com>
2024-02-26 21:57:52 +0530

    Created Signup page

--- frontend/src/Components/Signup.js
+++ frontend/src/Components/Signup.js
@@ -3,8 +3,50 @@ import "../Styles/Signup.css"
 
 function Signup() {
   return (
-    <div>
-        {/* Signup page here */}
+    <div className='outerSignup'>
+        <div class="wrapper">
+        <form action="">
+            <h1>Register</h1>
+            <div class="input-box">
+                <input class="inp" type="text" required/>
+                <label>Name</label>
+                <i class='bx bxs-user'></i>
+            </div>
+            <div class="input-box">
+                <input class="inp" type="text" required/>
+                <label>Email</label>
+                <i class='bx bxs-envelope'></i>
+                
+            </div>
+            <div class="input-box">
+                <input type="password" required/>
+                <label>Password</label>
+                <i class='bx bx-lock'></i>
+            </div>
+            
+            <div class="gender">
+                <h3>Gender</h3>
+                <div class="inpGender">
+
+                    <label id="Male"><input name="gender" type="radio"/>Male</label>
+                    <label id="Female"><input name="gender" type="radio"/>Female</label>
+                </div>
+            </div>
+            
+            <div class="input-box">
+                <input class="inp" type="text" required/>
+                <label>Phone No.</label>
+                <i class='bx bxs-phone'></i>
+            </div>
+            <div class="input-box">
+                <input class="inp" type="text" required/>
+                <label>Hobby</label>
+                <i class='bx bxs-bookmarks'></i>
+            </div>
+
+            <button type="Submit" class="btn">Sign-up</button>
+        </form>
+    </div>
     </div>
   )
 }
